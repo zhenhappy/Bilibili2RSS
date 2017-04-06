fb644f81fb6902ab5c0801af8635dba0c6e33a2c
wdssmq <wdssmq@qq.com>
2017-04-06 14:42:59 +0800

    对ed2k获取进行限制；

--- BT/function.php
+++ BT/function.php
@@ -8,13 +8,24 @@ function GetVars($name, $type = 'REQUEST')
     return null;
   }
 }
-function SaveFile($tmp,$path,$fn) {
-    if (in_array(strtoupper(PHP_OS), array('WINNT', 'WIN32', 'WINDOWS'))) {
-        $fn = iconv("UTF-8", "GBK//IGNORE", $fn);
-    }
-    @move_uploaded_file($tmp, $path . $fn);
-
-    return $path . $fn;
+function GetGuestIP()
+{
+  return GetVars("REMOTE_ADDR", "SERVER");
 }
-
+function GetGuestAgent()
+{
+  return GetVars("HTTP_USER_AGENT", "SERVER");
+}
+function SaveFile($tmp, $path, $fn)
+{
+  if (in_array(strtoupper(PHP_OS), array(
+    'WINNT',
+    'WIN32',
+    'WINDOWS'
+  ))) {
+    $fn = iconv("UTF-8", "GBK//IGNORE", $fn);
+  }
+  @move_uploaded_file($tmp, $path . $fn);
+  return $path . $fn;
+}
 ?>
--- BT/get.php
+++ BT/get.php
@@ -1,8 +1,9 @@
 <?php
 header("Content-type:text/json");
 require_once 'function.php';
+require '../class/ua_json.php';
 // if(GetVars("check","GET") === GetVars("torrentName","COOKIE"))
-  // echo 65537;
+// echo 65537;
 if (count($_FILES) == 0)
   die();
 $fileTorrent = "";
@@ -10,7 +11,7 @@ if ($_FILES["torrent"]["error"] > 0) {
   echo "{error: '" . $_FILES["torrent"]["error"] . "'}";
   die();
 } elseif ($_FILES["torrent"]["type"] == "application/x-bittorrent") {
-  setcookie("torrentName",$_FILES["torrent"]["name"]);
+  setcookie("torrentName", $_FILES["torrent"]["name"]);
   // echo "Upload: " . $_FILES["torrent"]["name"];
   // echo "<br>Type: " . $_FILES["torrent"]["type"];
   // echo "<br>Size: " . ($_FILES["torrent"]["size"] / 1024) . " Kb";
@@ -25,41 +26,47 @@ if ($_FILES["torrent"]["error"] > 0) {
 set_time_limit(0);
 require_once 'Torrent.php';
 // get torrent infos
-$torrent = new Torrent($fileTorrent);
+$torrent          = new Torrent($fileTorrent);
 // echo '<br><br>private: ', $torrent->is_private() ? 'yes' : 'no';
 // echo '<br><br>annonce: ', json_encode($torrent->announce());
 // echo '<br><br>name: ', $torrent->name();
 // if ($torrent->comment() != "")
-  // echo '<br><br>comment: ', $torrent->comment();
+// echo '<br><br>comment: ', $torrent->comment();
 // echo '<br><br>piece_length: ', $torrent->piece_length();
 // echo '<br><br>size: ',$torrent->size(2);
 // echo '<br><br>hash info: ', $torrent->hash_info();
 // echo '<br><br>magnet link: ', $torrent->magnet();
-
 //false会输出&而不是&amp;
 // echo '<br><br>magnet link: ', $torrent->magnet(false);
-
 // scrape()方法会尝试向所有Tracker查询当前种子的信息
 // var_dump($torrent->scrape());
-
 // echo '<br><br>content: ';
 // var_dump($torrent->content());
 // if (count($torrent->ed2k())>0)
-  // var_dump($torrent->ed2k());
-
-$objData = (object)[];
-$objData->name = $torrent->name();
+// var_dump($torrent->ed2k());
+$objData          = (object) array();
+$objData->name    = $torrent->name();
 $objData->comment = $torrent->comment();
-$objData->size = $torrent->size();
-$objData->magnet = "magnet:?xt=urn:btih:".$torrent->hash_info();
+$objData->size    = $torrent->size();
+$objData->magnet  = "magnet:?xt=urn:btih:" . $torrent->hash_info();
 // $objData->magnet = $torrent->magnet();
 $objData->content = $torrent->content();
-if (count($torrent->ed2k())>0)
+UA_JSON::Create();
+if (UA_JSON::Add() && count($torrent->ed2k()) > 0) {
   $objData->ed2k = $torrent->ed2k();
+} elseif (count($torrent->ed2k()) > 0) {
+  $objData->ed2k   = array();
+  $objData->ed2k[] = "1、当前种子含有ed2k信息，将本程序页面分享给至少3个人使用即可查看；";
+  $objData->ed2k[] = "2、使用未被记录的IP成功解析任意一个种子文件即为一次有效分享";
+  $objData->ed2k[] = "2、请保持当前的IP <b>" . GetGuestIP() . "</b>不变，否则将重新计数";
+}
 echo json_encode($objData);
 if (1 == 2) {
   // create torrent
-  $torrent = new Torrent( array( 'test.mp3', 'test.jpg' ), 'http://torrent.tracker/annonce' );
+  $torrent = new Torrent(array(
+    'test.mp3',
+    'test.jpg'
+  ), 'http://torrent.tracker/annonce');
   $torrent->announce(false); // reset announce trackers
   // modify torrent
   $torrent->announce('http://alternate-torrent.tracker/annonce'); // add a tracker
